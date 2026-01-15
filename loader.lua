0586f0de773b669a4438c48d27ff438049240e94
Eclypse <eclypse.zx7@gmail.com>
2026-01-14 22:51:24 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,8 @@
 local archives = {
-  2809202155 = "YBA.lua"
+  2809202155 = "YourBizarreAdventure.lua"
 }
 
 local placeId = game.PlaceId
-loadstring(
+
+local path = archives[placeId]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eclypseHz/EclypseHub/refs/heads/main/Sources/" .. path))()
